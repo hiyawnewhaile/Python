09a3252eed664b9aeea79af3abc748ab7288d58c
Hiyawnew Haile <hiyawnewhaile@gmail.com>
2022-05-06 00:53:37 -0400

    playground

---
+++ flask/fundamentals/assignment_playground/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/play')
+def index():
+    return render_template("index.html")
+
+@app.route('/play/<int:num>')
+def numbox(num):
+    return render_template("index2.html",num=num)
+
+@app.route('/play/<int:num>/<string:color>')
+def colorbox(num,color):
+    return render_template("index3.html",num=num,color=color)
+
+if __name__=="__main__":
+    app.run(debug=True)
